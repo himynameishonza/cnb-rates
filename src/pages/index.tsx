95a600f7ad3912e022e99f6eacd7945ed8e2850b
Jan Rádl <radl.honza@gmail.com>
2022-02-16 22:11:30 +0100

    Deploy to vercel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import styled from 'styled-components';
 import HeadMeta from '../components/HeadMeta';
 import PageHeader from '../components/PageHeader';
